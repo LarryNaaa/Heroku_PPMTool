f57cb8ea472b9f303fb6ed9b1cd7ff8fefbfd316
LarryNaaa <najinyu08@outlook.com>
2020-09-21 00:38:26 -0400

    react

---
+++ src/main/resources/static/precache-manifest.482d2931f96f9a8fb578406f2d631433.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec76213e15de366ea74d281d24e79cfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b14479794d1638ecf50",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "f4d3f1392d156ed7f94c",
+    "url": "/static/css/main.7556a164.chunk.css"
+  },
+  {
+    "revision": "7b14479794d1638ecf50",
+    "url": "/static/js/2.27db2764.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.27db2764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4d3f1392d156ed7f94c",
+    "url": "/static/js/main.b19f474f.chunk.js"
+  },
+  {
+    "revision": "f0395242fcfe40790f57",
+    "url": "/static/js/runtime-main.85b40700.js"
+  }
+]);
